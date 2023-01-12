e4a2de92f41c5c4c8de2dba35e727e9b1aee3862
Orest Bida <dev@orestbida.com>
2023-01-12 01:46:20 +0100

    Update demo example: cleanup

--- demo/app.js
+++ demo/app.js
@@ -4,14 +4,8 @@ const im = iframemanager();
 
 im.run({
 
-    /**
-     * @param {{
-     *  acceptedServices: string[]
-     *  eventSource: 'click' | 'api'
-     * }}
-     */
-    onChange: ({acceptedServices, eventSource}) => {
-        console.log(eventSource, acceptedServices)
+    onChange: ({state, eventSource}) => {
+        console.log(state, eventSource)
     },
 
     currLang: 'en',
@@ -192,180 +186,10 @@ im.run({
                 // The map, centered at Uluru
                 const map = new google.maps.Map(div.querySelector('.map'), {
                     zoom: 4,
-                    center: uluru,
-                    styles: [
-                        {
-                            "featureType": "all",
-                            "elementType": "geometry.fill",
-                            "stylers": [
-                                {
-                                    "weight": "2.00"
-                                }
-                            ]
-                        },
-                        {
-                            "featureType": "all",
-                            "elementType": "geometry.stroke",
-                            "stylers": [
-                                {
-                                    "color": "#9c9c9c"
-                                }
-                            ]
-                        },
-                        {
-                            "featureType": "all",
-                            "elementType": "labels.text",
-                            "stylers": [
-                                {
-                                    "visibility": "on"
-                                }
-                            ]
-                        },
-                        {
-                            "featureType": "landscape",
-                            "elementType": "all",
-                            "stylers": [
-                                {
-                                    "color": "#f2f2f2"
-                                }
-                            ]
-                        },
-                        {
-                            "featureType": "landscape",
-                            "elementType": "geometry.fill",
-                            "stylers": [
-                                {
-                                    "color": "#ffffff"
-                                }
-                            ]
-                        },
-                        {
-                            "featureType": "landscape.man_made",
-                            "elementType": "geometry.fill",
-                            "stylers": [
-                                {
-                                    "color": "#ffffff"
-                                }
-                            ]
-                        },
-                        {
-                            "featureType": "poi",
-                            "elementType": "all",
-                            "stylers": [
-                                {
-                                    "visibility": "off"
-                                }
-                            ]
-                        },
-                        {
-                            "featureType": "road",
-                            "elementType": "all",
-                            "stylers": [
-                                {
-                                    "saturation": -100
-                                },
-                                {
-                                    "lightness": 45
-                                }
-                            ]
-                        },
-                        {
-                            "featureType": "road",
-                            "elementType": "geometry.fill",
-                            "stylers": [
-                                {
-                                    "color": "#eeeeee"
-                                }
-                            ]
-                        },
-                        {
-                            "featureType": "road",
-                            "elementType": "labels.text.fill",
-                            "stylers": [
-                                {
-                                    "color": "#7b7b7b"
-                                }
-                            ]
-                        },
-                        {
-                            "featureType": "road",
-                            "elementType": "labels.text.stroke",
-                            "stylers": [
-                                {
-                                    "color": "#ffffff"
-                                }
-                            ]
-                        },
-                        {
-                            "featureType": "road.highway",
-                            "elementType": "all",
-                            "stylers": [
-                                {
-                                    "visibility": "simplified"
-                                }
-                            ]
-                        },
-                        {
-                            "featureType": "road.arterial",
-                            "elementType": "labels.icon",
-                            "stylers": [
-                                {
-                                    "visibility": "off"
-                                }
-                            ]
-                        },
-                        {
-                            "featureType": "transit",
-                            "elementType": "all",
-                            "stylers": [
-                                {
-                                    "visibility": "off"
-                                }
-                            ]
-                        },
-                        {
-                            "featureType": "water",
-                            "elementType": "all",
-                            "stylers": [
-                                {
-                                    "color": "#46bcec"
-                                },
-                                {
-                                    "visibility": "on"
-                                }
-                            ]
-                        },
-                        {
-                            "featureType": "water",
-                            "elementType": "geometry.fill",
-                            "stylers": [
-                                {
-                                    "color": "#c8d7d4"
-                                }
-                            ]
-                        },
-                        {
-                            "featureType": "water",
-                            "elementType": "labels.text.fill",
-                            "stylers": [
-                                {
-                                    "color": "#070707"
-                                }
-                            ]
-                        },
-                        {
-                            "featureType": "water",
-                            "elementType": "labels.text.stroke",
-                            "stylers": [
-                                {
-                                    "color": "#ffffff"
-                                }
-                            ]
-                        }
-                    ]
+                    center: uluru
                 });
 
-                if(div.dataset.mapsMarker === ""){
+                if(div.dataset.mapsMarker === ''){
                     // The marker, positioned at Uluru
                     const marker = new google.maps.Marker({
                         position: uluru,
@@ -373,7 +197,7 @@ im.run({
                     });
                 }
 
-                if(div.dataset.mapsStreetview === ""){
+                if(div.dataset.mapsStreetview === ''){
                     const panorama = new google.maps.StreetViewPanorama(div.querySelector('.map'), {
                             position: uluru,
                             pov: {
